44fb7fcba2f8767973dd68e37005c146f4ab1612
Janus Lynggaard Thorborg <janus.thorborg@gmail.com>
2016-10-21 15:27:59 +0200

    Small juce compability fix

--- gui/CToolTip.cpp
+++ gui/CToolTip.cpp
@@ -80,11 +80,14 @@ namespace cpl
 	void CToolTipWindow::updatePosition (const String& tip, Point<int> pos, const juce::Rectangle<int>& parentArea)
 	{
 		int w, h;
-		auto bounds = getLookAndFeel().getTooltipBounds (tip, pos, parentArea);
+		#if JUCE_MAJOR_VERSION >= 4
+			auto bounds = getLookAndFeel().getTooltipBounds (tip, pos, parentArea);
 		
-		w = bounds.getWidth();
-		h = bounds.getHeight();
-
+			w = bounds.getWidth();
+			h = bounds.getHeight();
+		#else
+			getLookAndFeel().getTooltipSize(tip, w, h);
+		#endif
 		setBounds (juce::Rectangle<int> (pos.x > parentArea.getCentreX() ? pos.x - (w + 12) : pos.x + 24,
 								   pos.y > parentArea.getCentreY() ? pos.y - (h + 6)  : pos.y + 6,
 								   w, h)
